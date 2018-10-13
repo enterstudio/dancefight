7e8f5a0838c0bab85f0ab5c866d8678c7925a689
Jonathan Eagles <jeagles@yahoo-inc.com>
2018-10-13 17:24:01 -0500

    Center title art desciption and snark

--- even-bigger.rb
+++ even-bigger.rb
@@ -1,10 +1,8 @@
 require 'squib'
-require 'game_icons'
 
 deck = Squib.xlsx file: 'even-bigger.xlsx', strip: false, sheet: 0
 
-Squib::Deck.new(cards: deck.nrows, layout: %w(hand.yml even-bigger.yml)) do
-  #background color: '#230602'
+Squib::Deck.new(cards: deck.nrows, layout: %w(dance-layout.yml even-bigger.yml)) do
   background color: '#ffffff'
   svg file: deck['Art'], layout: 'Art'
 
@@ -12,22 +10,12 @@ Squib::Deck.new(cards: deck.nrows, layout: %w(hand.yml even-bigger.yml)) do
     text str: deck[key], layout: key
   end
 
-  %w(Punch-Blast G-Clef Glass-Heart).each do |key|
-    svg data: GameIcons::Icon.new("#{key.downcase}.svg").string.gsub(':#ffffff', 'colorswap').gsub(':#230602', ':#ffffff').gsub('colorswap', ':#230602'), layout: "#{key}Icon"
+  %w(Punch-Blast G-Clef).each do |key|
+    svg file: "#{key.downcase}.svg", layout: "#{key}Icon"
     text str: deck[key], layout: key
   end
-  save_pdf(
-	  trim: 0,
-	  margin: 0,
-	  gap: 0,
-	  columns: 3,
-	  rows: 3
-  )
-  save_sheet(
-	  trim: 0,
-	  margin: 0,
-	  gap: 0,
-	  columns: 3,
-	  rows: 3
-  )
+  rect layout: 'cut'
+  rect layout: 'safe'
+  save_pdf(trim: 0, margin: 0, gap: 0, columns: 3, rows: 3)
+  save_sheet(trim: 0, margin: 0, gap: 0, columns: 3, rows: 3)
 end
